0a44bf571915fb19c205516caa020e2828ae3761
Florian Eckert <fleckert@microsoft.com>
2021-11-25 14:31:55 +0100

    without 'jq'

--- posts/azure-functions-function-access-keys/getFunctionAccessKeys.sh
+++ posts/azure-functions-function-access-keys/getFunctionAccessKeys.sh
@@ -9,13 +9,13 @@ functionAppName="$2"
 url="https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Web/sites/$functionAppName/functions?api-version=2021-03-01"
 
 echo "---------------------------------------------------------------------------------"
-echo "key                                                      |   functionName"
+echo "function access key                                      | functionName"
 echo "---------------------------------------------------------|-----------------------"
-for functionName in $(az rest --url $url | jq -r .value[].properties.name);
+for functionName in $(az rest --url $url --query "value[].properties.name" --output tsv);
 do 
-  key=$(\
-    az functionapp function keys list --resource-group $resourceGroupName --name $functionAppName --function-name $functionName | jq -r .default
-  )
+  url="https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Web/sites/$functionAppName/functions/$functionName/listkeys?api-version=2021-03-01"
+  
+  key=$(az rest --method POST --url $url --query "default" --output tsv)
 
   echo "$key | $functionName"
 done
