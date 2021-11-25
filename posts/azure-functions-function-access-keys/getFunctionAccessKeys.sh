dfb11ea6499173d99bf4e5a6cbed491de2618e4f
Florian Eckert <fleckert@microsoft.com>
2021-11-25 14:50:50 +0100

    documentation

--- posts/azure-functions-function-access-keys/getFunctionAccessKeys.sh
+++ posts/azure-functions-function-access-keys/getFunctionAccessKeys.sh
@@ -5,7 +5,8 @@ set -e
 subscriptionId=$(az account show --output tsv --query id)
 resourceGroupName="$1"
 functionAppName="$2"
- 
+
+# https://docs.microsoft.com/en-us/rest/api/appservice/web-apps/list-functions
 url="https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Web/sites/$functionAppName/functions?api-version=2021-03-01"
 
 echo "---------------------------------------------------------------------------------"
@@ -13,6 +14,7 @@ echo "function access key                                      | functionName"
 echo "---------------------------------------------------------|-----------------------"
 for functionName in $(az rest --url $url --query "value[].properties.name" --output tsv);
 do 
+  # https://docs.microsoft.com/en-us/rest/api/appservice/web-apps/list-function-keys
   url="https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Web/sites/$functionAppName/functions/$functionName/listkeys?api-version=2021-03-01"
   
   key=$(az rest --method POST --url $url --query "default" --output tsv)
